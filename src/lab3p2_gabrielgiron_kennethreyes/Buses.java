84ee6efd512880337f9582f697490be206efc1ad
Kenneth Daniel Reyes <dan29reyes@unitec.edu>
2022-02-04 19:21:13 -0600

    Avance7

---
+++ src/lab3p2_gabrielgiron_kennethreyes/Buses.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3p2_gabrielgiron_kennethreyes;
+
+/**
+ *
+ * @author Daniel
+ */
+
+public class Buses extends Transporte {
+
+    private int sillas;
+    private int pie;
+
+    public Buses(int sillas, int pie, String placa, String color) {
+        super(placa, color);
+        this.sillas = sillas;
+        this.pie = pie;
+    }
+    
+    public int getSillas() {
+        return sillas;
+    }
+
+    public void setSillas(int sillas) {
+        this.sillas = sillas;
+    }
+
+    public int getPie() {
+        return pie;
+    }
+
+    public void setPie(int pie) {
+        this.pie = pie;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo: Bus: "+super.toString();
+    }
+
+}
---
+++ src/lab3p2_gabrielgiron_kennethreyes/Taxi.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3p2_gabrielgiron_kennethreyes;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Taxi extends Transporte{
+
+    final int capacidad = 4;
+    private String idTaxi;
+
+    public Taxi(String id, String placa, String color) {
+        super(placa, color);
+        this.idTaxi = id;
+    }
+    
+    public int getCapacidad() {
+        return capacidad;
+    }
+    
+    public String getIdTaxi() {
+        return idTaxi;
+    }
+
+    public void setId(String idTaxi) {
+        this.idTaxi = idTaxi;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo: Taxi"+super.toString();
+    }
+
+}
